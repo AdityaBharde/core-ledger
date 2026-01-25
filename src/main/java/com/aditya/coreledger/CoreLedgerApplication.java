d3f5a7a79d9ae8ace8c2ce082e5acf6a57258c97
Aditya <adityabharde05@gmail.com>
2026-01-25 20:29:51 +0530

    Initial commit

---
+++ src/main/java/com/aditya/coreledger/CoreLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.coreledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreLedgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aditya/coreledger/CoreLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.coreledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
